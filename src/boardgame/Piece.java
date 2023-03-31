a106150a5be339529c74499e6fc6a34292a37a60
Leonardo Alves <leonardo.tenorio@outlook.com>
2023-03-31 20:54:30 -0300

    Start to implement board and piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,20 @@
+package boardgame;
+
+public class Piece {
+	
+	protected Position position;// Peça fica como null, por isso não entra no construtor
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board; // O construtor tem apenas o tabuleiro
+		position = null; // Se não colocar, o java já atribui como null
+	}
+	// Somente classes do mesmo pacote e subclasses podem acessar esse método, por isso protected
+	protected Board getBoard() {// Usa-se somente o get pois não instancia outro tabuleiro
+		return board;
+	}
+
+	
+	
+
+}
